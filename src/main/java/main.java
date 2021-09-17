36c3650eba59e41941d4b7447716c3e69f6a41f6
iwanlevin <apple.iwan@gmail.com>
2021-09-17 06:53:45 -0700

    added a method for  sorting strings alphabetically and the choice  to enter either string or integer.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,40 +5,65 @@ public class main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
-        int input = 1;
-        System.out.printf("Enter an Integers to add to the list(Enter -1 to exit): ");
+        ArrayList<String> liststr = new ArrayList<>();
 
-        while(input > 0){
-            input = in.nextInt();
-            if(input < 0){
-                break;
-            }else{
-                list.add(input);
+        int inputint = 1;
+        String inputstr = "";
+        String choice = in.nextLine();
+
+        if (choice.equals("s") || choice.equals("S")){
+            System.out.print("Enter an Strings to add to the list(Enter exit to quite): ");
+
+            while (!inputstr.equals("exit")) {
+                inputstr = in.nextLine();
+                if (inputstr.equals("exit")) {
+                    break;
+                } else {
+                    liststr.add(inputstr);
+                }
             }
-        }
 
-        int[] arr = new int[list.size()];
-        int i = 0;
-        for (Integer x: list) {
-            arr[i] = x;
-            i++;
-        }
-        int[] sortedArray = quickSort(arr, 0, arr.length - 1);
+            String[] arr = new String[liststr.size()];
+            int i = 0;
+            for (String x : liststr) {
+                arr[i] = x;
+                i++;
+            }
+            String[] sortedArray = stringSort(arr);
 
-        System.out.print(Arrays.toString(sortedArray));
+            System.out.print(Arrays.toString(sortedArray));
+        }else {
+            System.out.print("Enter an Integers to add to the list(Enter -1 to quite): ");
+
+            while (inputint > 0) {
+                inputint = in.nextInt();
+                if (inputint < 0) {
+                    break;
+                } else {
+                    list.add(inputint);
+                }
+            }
+
+            int[] arr = new int[list.size()];
+            int i = 0;
+            for (Integer x : list) {
+                arr[i] = x;
+                i++;
+            }
+            int[] sortedArray = quickSort(arr, 0, arr.length - 1);
+
+            System.out.print(Arrays.toString(sortedArray));
+        }
     }
 
 
-
-    public static int[] quickSort(int[] list,int li,int ui){
+    public static int[] quickSort(int[] list, int li, int ui) {
         if (li < ui) {
             int pivot = list[ui];
             int i = (li - 1);
 
-            for (int l = li; l <= ui - 1; l++)
-            {
-                if (list[l] < pivot)
-                {
+            for (int l = li; l <= ui - 1; l++) {
+                if (list[l] < pivot) {
                     i++;
                     int temp = list[i];
                     list[i] = list[l];
@@ -58,4 +83,28 @@ public class main {
 
         return list;
     }
+
+
+    public static String[] stringSort(String[] list) {
+
+        int size = list.length;
+        for(int i = 0; i<size-1; i++)
+        {
+            for (int j = i+1; j< list.length; j++)
+            {
+
+                if(list[i].compareTo(list[j])>0)
+                {
+
+                    String temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+
+
+        return  list;
+    }
+
 }
